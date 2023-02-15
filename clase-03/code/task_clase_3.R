7acb8be56e2818f9f63bbba7f5be3c94b74bfaff
Eduard Fernando Martinez Gonzalez <edfemagonza@gmail.com>
2023-02-15 16:58:47 -0500

    update inital0-setup

---
+++ clase-03/code/task_clase_3.R
@@ -0,0 +1,82 @@
+#-----------------------------------------#
+# Universidad de los Andes
+# Taller de R Estadistica y Programación
+# 2023-01
+# Task Clase 3: Vectores,Matrices y Dataframes
+# Eduard F Martinez
+
+#------------------------------------------#
+# En el siguiente script se le solicitará 
+# realizar ciertas tareas con la finalidad 
+# de afianzar su conocimento en el 
+# manejo de estructuras en R.
+
+#-------------------------------------------#
+# 1. Genere un vector llamado 'my_space',
+# el cual tendrá 4 elementos en su interior:
+# "marte","jupiter","tierra","luna", asegurese 
+# de seguir ese orden. 
+
+
+#---------------------------------------------#
+# 2. Utilice al menos 3 formas de diferentes 
+# para quedarse solo con el elemento "luna".
+
+
+#-------------------------------------------------#
+# 3. Reorganice la posición de los elementos
+# dentro del vector, asignando el verdadero orden 
+# de los cuerpos celestes en el sistema solar como 
+# las nuevas posiciones, asegurese de guardar los 
+# cambios nuevamente en el objeto 'my_space'.
+
+
+#--------------------------------------#
+#4. Cree un vector llamado 'odds',
+# con los numeros impares del 1 al 8
+# y otro llamado 'evens' con los
+# numeros pares del 1 al 4
+# hint: Puede hacerlo de manera manual
+# o puede usar la funcion seq().
+
+
+#-------------------------------------#
+# 5. Combine los dos vectores que 
+# se pueden unir en uno solo,
+# llamelo 'final_vector'.
+
+
+#----------------------------------------#
+# 5. Cree una matriz usando solo
+# los dos vectores que se pueden combinar 
+# dentro de esta y llamaela 'the_matrix'.
+
+
+#-----------------------------------------#
+# 6. Imprima en la consola el elemento 
+# que está en la fila 3 y en la columna 2 
+# de la matriz. 
+
+
+#---------------------------------------#
+# 7. Convierta en NA, todos los elementos
+# impares de la matriz
+
+
+#--------------------------------------#
+# 8. Cree un data frame con todos los 
+# vectores que tengan la misma longitud,
+# llamelo 'my_data'.
+
+
+#------------------------------------------#
+# 9. Calcule la suma de cada uno de las 
+# observaciones de las variables numericas,
+
+
+#------------------------------------#
+# 10. Elimine la variable cuyos
+# elementos sumen la menor cantidad.
+
+
+
---
+++ clase-04/code/task_clase_4.R
@@ -0,0 +1,64 @@
+#-----------------------------------------#
+# Universidad de los Andes
+# Taller de R Estadistica y Programación
+# 2023-01
+# Task Clase 4: Listas, Directorios de Trabajo
+# cargar bases de datos y EDA
+# Eduard F Martinez
+
+#--------------------------------------#
+# 1. ¿Que es un directorio de trabajo?
+
+
+#----------------------------------------#
+# 2. Imprima en la consola cual es el directorio de trabajo
+# actual que está siendo empleado por R
+
+
+#---------------------------------------#
+# 3. Reasigne el directorio de trabajo a 
+# 2_task-clase-4
+
+
+#----------------------------------------#
+# 4. Cargue un objeto de nombre df el 
+# archi gapminder que se encuentra en
+# la carpeta input. 
+# hint: no olvide cargar la libreria rio
+
+
+#-----------------------------------------#
+#5. Export el objeto df en formato .rds a la 
+# carpeta output, llamelo 'gapminder.rds'
+
+
+#------------------------------------------#
+# 6. Cree las siguientes estructuras:
+# 1. Vector llamado 'vowels' que contenga las vocales.
+# 2. Matriz llamada 'inputs' con dimensiones 2x2 con los numeros del 1 al 4
+# 3.  Un dataframe llamado 'db' que contega como variables:
+#     - El vector 'vowels'
+#     - Un vector llamado 'numbers' que
+#       tenga los numeros del 1 al 5
+
+
+#--------------------------------------------#
+# 7. Guarde las estructuras previamente
+# creadas en una lista llamada 'collection' 
+
+
+#--------------------------------------------#
+# 8. Nombre cada elemento dentro de la lista
+# 'collection' como el nombre de la estructura en R.
+
+
+#---------------------------------------------#
+# 9. Imprima solo el elemento 2 
+# dentro de la lista 'collection
+
+
+#---------------------------------------------#
+# 10. Imprima solo la variable vowels del dataframe 
+# que se encuentra de la lista 'collection'
+
+
