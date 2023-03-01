cc12197bb1fc43f4cf24fccbb8676c92eec1a451
Taller de R <edfemagonza@gmail.com>
2023-03-01 14:04:56 -0500

    update

---
+++ clase-05/task_clase_05.R
@@ -0,0 +1,31 @@
+#-----------------------------------------#
+# Universidad de los Andes
+# Taller de R Estadistica y Programación
+# 2023-01
+# Task Clase 05: Manejo de datos
+# Eduard F Martinez
+
+
+#-------------------------------------#
+# 1. Carga el dataset mtcars y la libreria tidyverse
+
+#-------------------------------------#
+# 2. Crea una nueva variable llamada am_factor utilizando la función mutate(). 
+#    La variable deberá tomar el valor "Automatic" si el valor de am es igual a 0 y "Manual" 
+#    si el valor de am es igual a 1.
+
+#-------------------------------------#
+# 3. Crea una nueva variable llamada mpg_factor utilizando la función mutate().
+#    La variable deberá tomar el valor "High" si el valor de mpg es mayor a 20, "Medium" si el valor de mpg está entre 15 y 20 (inclusive) y 
+#   "Low" si el valor de mpg es menor a 15.
+
+#-------------------------------------#
+# 4. Selecciona las variables de mtcars que comiencen con mpg y asigne esto a un nuevo objeto
+
+#-------------------------------------#
+# 5. Selecciona la variable que son tipo character y asigne esto a un nuevo objeto
+
+#-------------------------------------#
+# 6. Deseleccione la variable "drat" y asigne esto a un nuevo objeto
+
+
---
+++ clase-06/task_clase_06.R
@@ -0,0 +1,47 @@
+#-----------------------------------------#
+# Universidad de los Andes
+# Taller de R Estadistica y Programación
+# 2023-01
+# Task Clase 06: Git
+# Git y GitHUB
+# Eduard F Martinez
+
+
+#----------------------------------#
+# 1. Crea un repositorio en Git en línea, utiliza la plataforma de github para alojar este repositorio.
+# Asegurese que su repositorio sea publico
+
+#----------------------------------#
+# 2. Una vez que hayas creado el repositorio en línea copie el HTTPS.
+
+#----------------------------------#
+#3.  Abra RStudio y cree un nuevo proyecto. En la ventana de creación de proyecto, selecciona "Version Control" y luego "Git".
+
+#----------------------------------#
+# 4. Pege la HTTPS del repositorio que creaste en línea en el campo "Repository URL". 
+# Asegúrate de seleccionar la carpeta donde deseas clonar el repositorio en tu computadora.
+# Haz clic en "Create Project" para crear el proyecto y clonar el repositorio en tu computadora.
+
+#----------------------------------#
+# 5. Crea un nuevo  script en el proyecto y dentro del script pondra su nombre, código, fecha y versión de R.
+
+#----------------------------------#
+# 6. Nombra y guarda el archivo dentro de una carpeta que se llamara scr, la cual se ubicara dentro del proyecto.
+
+#----------------------------------#
+# 7. Presiona la pestaña "Git" en el panel izquierdo de RStudio.
+
+#----------------------------------#
+# 8. Haz clic en el botón "commit"
+
+#----------------------------------#
+# 9. Seleciona el archivo que quieras subir. 
+
+#----------------------------------#
+# 10. Agrega un mensaje de commit en el campo "Commit message" y haz clic en "Commit".
+
+#----------------------------------#
+# 11. Haz clic en el botón "Push" para enviar los cambios al repositorio en línea.
+
+#----------------------------------#
+# Copie el URL de su repositorio y envielo atravez de bloque neon.
